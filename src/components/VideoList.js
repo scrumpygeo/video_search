92bf71005687c005db3af865a6d89e1380d228af
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-07 17:35:51 +0000

    output thumbnails and titles to VideoItem

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,16 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = props => {
-  return <div>{props.videos.length}</div>;
+// const VideoList = props => {
+//   return <div>{props.videos.length}</div>;
+// };  - destructure this to:
+
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map(video => {
+    return <VideoItem video={video} />;
+  });
+
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
