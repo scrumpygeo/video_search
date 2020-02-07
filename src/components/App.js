ef66ba6b12d5a62d7ad4646148578483d3ca12e9
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-07 17:04:04 +0000

    query youtube and return results

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,28 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+require("dotenv").config();
 
 class App extends React.Component {
-  onTermSubmit = term => {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
     // callback for when we submit search term to child component SearchBar
     // now we can call preconfigured instance of axios we called youtube
-    youtube.get("/search", {
+    const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
   };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
+        {this.state.videos.length} videos.
       </div>
     );
   }
