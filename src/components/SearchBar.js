d011a9fc6401e387748eabc0a52e722dcf66b975
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-07 13:12:30 +0000

    add event handlers to input and form to watch for text input and form submission

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,33 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" }; //search term
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFromSubmit = event => {
+    event.preventDefault();
+
+    // Todo: callback from parent component
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
